ac9308fa9c3d6d04d701b135558faf9666895e55
JONNAGADDALA RAVIKUMAR <ravikumarjit583@gmail.com>
2020-11-09 11:19:04 +0530

    Created LinkedList by Adding 30 and 56 to 70

--- jUnit/MyNodeTest.java
+++ jUnit/MyNodeTest.java
@@ -3,6 +3,7 @@ package jUnit;
 import org.junit.Assert;
 import org.junit.Test;
 import main.MyNode;
+import main.MyLinkedList;
 
 public class MyNodeTest {
 	
@@ -17,6 +18,25 @@ public class MyNodeTest {
 		
 		boolean result = myFirstNode.getNext().equals(mySecondNode)
 				&& mySecondNode.getNext().equals(myThirdNode);
+		Assert.assertTrue(result);
+	}
+	
+	@Test
+	public void shouldPassThisTestWhenAddingThreeValuesToLinkedList() {
+		MyNode<Integer> myFirstNode = new MyNode<>(70);
+		MyNode<Integer> mySecondNode = new MyNode<>(30);
+		MyNode<Integer> myThirdNode = new MyNode<>(56);
+		
+		MyLinkedList myLinkedList = new MyLinkedList();
+		
+		myLinkedList.add(myFirstNode);
+		myLinkedList.add(mySecondNode);
+		myLinkedList.add(myThirdNode);
+		
+		myLinkedList.printMyNode();
+		boolean result = myLinkedList.head.equals(myThirdNode) &&
+							myLinkedList.head.getNext().equals(mySecondNode) &&
+							myLinkedList.tail.equals(myFirstNode);
 		Assert.assertTrue(result);
 	}
 }	
--- main/MyNode.java
+++ main/MyNode.java
@@ -1,20 +1,29 @@
 package main;
 
-public class MyNode<K> {
+public class MyNode<K> implements INode<K>{
 	
 	private K key;
-	private MyNode<K> next;
+	private INode<K> next;
 
 	public MyNode(K key) {
 		this.key = key;
 		this.next = null;
 	}
+	
+	public K getKey() {
+		return key;
+	}
+	
+	public void setKey(K key) {
+		this.key = key;
+	}
 
-	public MyNode<K> getNext() {
+	public INode<K> getNext() {
 		return next;
 	}
 
-	public void setNext(MyNode<K> next) {
-		this.next = next;
+	@Override
+	public void setNext(INode<K> next) {
+		this.next = (MyNode<K>) next;
 	}
 }
