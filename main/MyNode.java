abc28e5e58694d0597b85521471e84afe2bbc3c7
JONNAGADDALA RAVIKUMAR <ravikumarjit583@gmail.com>
2020-11-09 09:58:33 +0530

    Simple LinkedList

---
+++ main/MyNode.java
@@ -0,0 +1,20 @@
+package main;
+
+public class MyNode<K> {
+	
+	private K key;
+	private MyNode<K> next;
+
+	public MyNode(K key) {
+		this.key = key;
+		this.next = null;
+	}
+
+	public MyNode<K> getNext() {
+		return next;
+	}
+
+	public void setNext(MyNode<K> next) {
+		this.next = next;
+	}
+}
