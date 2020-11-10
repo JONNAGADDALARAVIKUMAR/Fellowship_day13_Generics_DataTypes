5f6d82089b0345c83c0ff2011e35fbd6c2c2bfc7
JONNAGADDALA RAVIKUMAR <ravikumarjit583@gmail.com>
2020-11-10 11:06:11 +0530

    Added: Queue Appending by enQue Method and Internally Calling Append

---
+++ main/MyQueue.java
@@ -0,0 +1,18 @@
+package main;
+
+public class MyQueue {
+
+	private final MyLinkedList myLinkedList;
+	
+	public MyQueue() {
+		this.myLinkedList = new MyLinkedList();
+	}
+
+	public void enque(MyNode newNode) {
+		myLinkedList.append(newNode);
+	}
+
+	public void printMyNode() {
+		myLinkedList.printMyNode();
+	}
+}
